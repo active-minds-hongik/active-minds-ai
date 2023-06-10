708141becdb7428317155c1f8c5617d9a86c8e65
yjoonjang <yjoonjang34@gmail.com>
2023-06-10 18:10:23 +0900

    fix requirements.txt

--- app.py
+++ app.py
@@ -6,23 +6,27 @@ app = Flask("face_recognition")
 
 cors = CORS(app)
 
-@cross_origin()
-@app.route('/', methods=['GET'])
-def index():
-    return 'hi'
 
 @cross_origin()
-@app.route('/ai/example', methods=['POST'])
+@app.route("/", methods=["GET"])
+def index():
+    return "hi"
+
+
+@cross_origin()
+@app.route("/ai/example", methods=["POST"])
 def upload_file():
     try:
-        image_data = request.form.get('image')
-        result = DeepFace.analyze(img_path=image_data, actions=['emotion'])
+        image_data = request.form.get("image")
+        print(image_data)
+        result = DeepFace.analyze(img_path=image_data, actions=["emotion"])
         print(result)
 
-        return result[0]['dominant_emotion']
+        return result[0]["dominant_emotion"]
 
     except Exception as e:
         return str(e)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
